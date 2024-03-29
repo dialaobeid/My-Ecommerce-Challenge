777b9c2ee23c5ad23d85407acb5734214e310ee4
dobeid <dialajo9@gmail.com>
2024-03-28 22:13:30 -0700

    updated model files with proper paths and associations

--- models/Category.js
+++ models/Category.js
@@ -1,12 +1,20 @@
 const { Model, DataTypes } = require('sequelize');
-
 const sequelize = require('../config/connection.js');
 
 class Category extends Model {}
 
 Category.init(
   {
-    // define columns
+    // Define columns
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
   },
   {
     sequelize,
--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,12 +1,36 @@
 const { Model, DataTypes } = require('sequelize');
-
 const sequelize = require('../config/connection');
 
-class ProductTag extends Model {}
+class ProductTag extends Model {
+  // Define association
+  static associate(models) {
+    this.belongsTo(models.Product, { foreignKey: 'product_id' });
+    this.belongsTo(models.Tag, { foreignKey: 'tag_id' });
+  }
+}
 
 ProductTag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false // Doesn't allow null values
+    },
+    product_id: {
+      type: DataTypes.INTEGER, // Integer data type
+      references: {
+        model: 'product', // References the Product model
+        key: 'id' // References the id column of the Product model
+      }
+    },
+    tag_id: {
+      type: DataTypes.INTEGER, // Integer data type
+      references: {
+        model: 'tag', // References the Tag model
+        key: 'id' // References the id column of the Tag model
+      }
+    }
   },
   {
     sequelize,
